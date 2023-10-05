95689bb6d8a0811f3265c512d33ce3f79033e93c
elbadritarek <elbadritarek612@gmail.com>
2023-10-05 17:48:16 +0200

    create CustomCelander create customCelanderItem

--- lib/views/widgets/home_veiw_body.dart
+++ lib/views/widgets/home_veiw_body.dart
@@ -5,6 +5,62 @@ class honeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        customCelander(),
+      ],
+    );
+  }
+}
+
+class customCelander extends StatelessWidget {
+  const customCelander({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          celanderItem(),
+          celanderItem(),
+          celanderItem(),
+          celanderItem(),
+          celanderItem(),
+          celanderItem(),
+          celanderItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class celanderItem extends StatelessWidget {
+  const celanderItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.deepOrangeAccent,
+          borderRadius: BorderRadius.all(Radius.circular(9))),
+      height: 80,
+      width: 50,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          Text("Su"),
+          SizedBox(
+            height: 12,
+          ),
+          Text("1", style: TextStyle(fontSize: 18)),
+        ],
+      ),
+    );
   }
 }
