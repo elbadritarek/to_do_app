0fe0d657fce0d1a3b0c3ad6a2108be45eebdca38
elbadritarek <elbadritarek612@gmail.com>
2023-10-17 18:25:23 +0200

    create custom task item

--- lib/views/widgets/home_veiw_body.dart
+++ lib/views/widgets/home_veiw_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class honeViewBody extends StatelessWidget {
   const honeViewBody({super.key});
@@ -8,6 +9,65 @@ class honeViewBody extends StatelessWidget {
     return Column(
       children: [
         customCelander(),
+        SizedBox(
+          height: 10,
+        ),
+        customTaskItem(),
+      ],
+    );
+  }
+}
+
+class customTaskItem extends StatelessWidget {
+  const customTaskItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(onPressed: () {}, icon: Icon(FontAwesomeIcons.circle)),
+        Expanded(
+          child: Container(
+            margin: EdgeInsets.only(right: 11),
+            padding: EdgeInsets.only(left: 5),
+            decoration: BoxDecoration(
+              color: Colors.blueAccent,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            width: double.maxFinite,
+            child: Row(
+              children: [
+                SizedBox(
+                  height: 20,
+                  child: VerticalDivider(
+                    color: Colors.orangeAccent,
+                    thickness: 5,
+                    width: 5,
+                  ),
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                Container(
+                  child: Column(
+                    children: [
+                      SizedBox(height: 5),
+                      Text("Flutter study plan",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontStyle: FontStyle.italic)),
+                      SizedBox(height: 5),
+                      Text("study flutter every day ",
+                          style: TextStyle(color: Colors.black54)),
+                      SizedBox(height: 5),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
       ],
     );
   }
@@ -18,20 +78,17 @@ class customCelander extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          celanderItem(),
-          celanderItem(),
-          celanderItem(),
-          celanderItem(),
-          celanderItem(),
-          celanderItem(),
-          celanderItem(),
-        ],
-      ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        celanderItem(),
+        celanderItem(),
+        celanderItem(),
+        celanderItem(),
+        celanderItem(),
+        celanderItem(),
+        celanderItem(),
+      ],
     );
   }
 }
