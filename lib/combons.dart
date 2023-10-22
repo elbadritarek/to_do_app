4f1108615b41f78705cf4d25a84f7c5417891c4e
elbadritarek <elbadritarek612@gmail.com>
2023-10-22 15:36:20 +0200

    add hive & create task_model.g &make init & openB

--- lib/combons.dart
+++ lib/combons.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const kprimary1Colour = Color(0xff38c2ce);
 const kprimary2Colour = Color(0xffcbe5e4);
+const kTaskBox = 'taskBox';
--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
+part 'task_model.g.dart';
+
+@HiveType(typeId: 1)
 class TaskModel {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String descrption;
+  @HiveField(2)
   final DateTime dateTime;
+  @HiveField(3)
   final TimeOfDay time;
+  @HiveField(4)
   final int colour;
 
   TaskModel({
