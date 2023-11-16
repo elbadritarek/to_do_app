52dc3b48fd7d3ca245f30109dafdee736fa1f1b4
elbadritarek <elbadritarek612@gmail.com>
2023-11-16 01:38:26 +0100

    add color to buttonAppBar

--- lib/views/widgets/bottom_appbar.dart
+++ lib/views/widgets/bottom_appbar.dart
@@ -1,42 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/combons.dart';
 
-class bottomAppBar extends StatelessWidget {
+class bottomAppBar extends StatefulWidget {
   const bottomAppBar({super.key});
 
   @override
+  State<bottomAppBar> createState() => _bottomAppBarState();
+}
+
+class _bottomAppBarState extends State<bottomAppBar> {
+  int selectIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Spacer(flex: 1),
-        IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.home,
-              size: 30,
-            )),
-        Spacer(flex: 2),
-        IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.calendar_month,
-              size: 30,
-            )),
-        Spacer(flex: 3),
-        IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.notifications_none_sharp,
-              size: 30,
-            )),
-        Spacer(flex: 2),
-        IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.person,
-              size: 30,
-            )),
-        Spacer(flex: 1),
-      ],
-    );
+    return Stack(children: [
+      /*AnimatedPositioned(
+          child: Container(
+            height: 50,
+            width: 90,
+            color: kprimary2Colour,
+          ),
+          duration: Duration(milliseconds: 500),
+          curve: Curves.easeInOut),*/
+      Row(
+        children: [
+          Spacer(flex: 1),
+          Container(
+            height: 50,
+            width: 60,
+            color: selectIndex == 0 ? kprimary2Colour : Colors.white,
+            child: IconButton(
+                onPressed: () {
+                  selectIndex == 0;
+                  setState(() {
+                    selectIndex = 0;
+                  });
+                },
+                icon: Icon(
+                  Icons.home,
+                  size: 30,
+                )),
+          ),
+          Spacer(flex: 2),
+          Container(
+            height: 50,
+            width: 60,
+            color: selectIndex == 1 ? kprimary2Colour : Colors.white,
+            child: IconButton(
+                onPressed: () {
+                  selectIndex == 1;
+                  setState(() {
+                    selectIndex = 1;
+                  });
+                },
+                icon: Icon(
+                  Icons.calendar_month,
+                  size: 30,
+                )),
+          ),
+          Spacer(flex: 3),
+          Container(
+            height: 50,
+            width: 60,
+            color: selectIndex == 2 ? kprimary2Colour : Colors.white,
+            child: IconButton(
+                onPressed: () {
+                  selectIndex == 2;
+                  setState(() {
+                    selectIndex = 2;
+                  });
+                },
+                icon: Icon(
+                  Icons.notifications_none_sharp,
+                  size: 30,
+                )),
+          ),
+          Spacer(flex: 2),
+          Container(
+            height: 50,
+            width: 60,
+            color: selectIndex == 3 ? kprimary2Colour : Colors.white,
+            child: IconButton(
+                onPressed: () {
+                  selectIndex == 3;
+                  setState(() {
+                    selectIndex = 3;
+                  });
+                },
+                icon: Icon(
+                  Icons.person,
+                  size: 30,
+                )),
+          ),
+          Spacer(flex: 1),
+        ],
+      ),
+    ]);
   }
 }
