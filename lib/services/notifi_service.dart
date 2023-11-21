3123e0b1298421b3db3582de0931ff1a2bd0699f
elbadritarek <elbadritarek612@gmail.com>
2023-11-22 00:51:27 +0100

    change in ios & android and create service notifi

---
+++ lib/services/notifi_service.dart
@@ -0,0 +1,39 @@
+//import 'dart:ffi';
+
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationServices {
+  final FlutterLocalNotificationsPlugin notificationPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  Future<void> initNotification() async {
+    AndroidInitializationSettings androidInitializationSettings =
+        const AndroidInitializationSettings("flutter_logo");
+
+    var initializationSettingsIos = DarwinInitializationSettings(
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestSoundPermission: true,
+      onDidReceiveLocalNotification: (id, title, body, payload) {},
+    );
+
+    var initializationSettings = InitializationSettings(
+        iOS: initializationSettingsIos, android: androidInitializationSettings);
+    await notificationPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: (details) {},
+    );
+  }
+
+  notificationDetails() {
+    return const NotificationDetails(
+        android: AndroidNotificationDetails("channelId", "channelName",
+            importance: Importance.max),
+        iOS: DarwinNotificationDetails());
+  }
+
+  Future showNotification(
+      {int id = 0, String? title, String? body, String? payload}) async {
+    return notificationPlugin.show(id, title, body, notificationDetails());
+  }
+}
